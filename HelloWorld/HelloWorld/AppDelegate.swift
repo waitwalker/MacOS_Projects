ac5ff6f88a777b65a80754dd4f49296107bba9ab
waitwalker <waitwalker@163.com>
2020-03-24 14:24:28 +0800

    hello world 添加

---
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by etiantian on 2020/3/24.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
