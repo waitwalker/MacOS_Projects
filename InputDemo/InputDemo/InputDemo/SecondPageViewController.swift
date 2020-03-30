e061215d341f39673436fe90708047ef3ec8fb2a
waitwalker <waitwalker@163.com>
2020-03-30 09:59:37 +0800

    登录界面

---
+++ InputDemo/InputDemo/InputDemo/SecondPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondPageViewController.swift
+//  InputDemo
+//
+//  Created by etiantian on 2020/3/30.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class SecondPageViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- InputDemo/InputDemo/InputDemo/ViewController.swift
+++ InputDemo/InputDemo/InputDemo/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import Cocoa
+import SnapKit
 
 class ViewController: NSViewController {
 
+    var accountLabel:NSText!
+    var accountTextField:NSTextField!
+    
+    var passwordLabel:NSText!
+    var passwordTextField:NSTextField!
+    
+    var loginButton:NSButton!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let textField:NSTextField = NSTextField(frame: NSRect(x: 100, y: 100, width: 200, height: 60))
-        textField.textColor = NSColor.red
-        self.view.addSubview(textField)
+        setupSubviews()
+        layoutSubviews()
         
         print("视图加载完成")
     }
+    
+    private func setupSubviews() -> Void {
+        accountTextField = NSTextField()
+        accountTextField.textColor = NSColor.red
+        accountTextField.placeholderString = "输入账号"
+        self.view.addSubview(accountTextField)
+        
+        passwordTextField = NSTextField()
+        passwordTextField.textColor = NSColor.red
+        passwordTextField.placeholderString = "输入密码"
+        self.view.addSubview(passwordTextField)
+        
+        loginButton = NSButton(title: "登录", target: self, action: #selector(loginButtonAction(button:)))
+        self.view.addSubview(loginButton)
+    }
+    
+    private func layoutSubviews() -> Void {
+        
+        accountTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(80)
+            make.right.equalTo(-80)
+            make.height.equalTo(50)
+            make.centerY.equalTo(self.view).offset(-130)
+        }
+        
+        passwordTextField.snp.makeConstraints { (make) in
+            make.left.right.height.equalTo(accountTextField)
+            make.top.equalTo(accountTextField.snp.bottom).offset(30)
+        }
+        
+        loginButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(50)
+            make.left.equalTo(50)
+            make.right.equalTo(-50)
+            make.height.equalTo(50)
+        }
+    }
+    
+    @objc func loginButtonAction(button:NSButton) {
+        print("登录按钮被点击")
+        let secondVC = SecondPageViewController()
+        self.presentAsModalWindow(secondVC)
+        
+    }
+    
+    
     
     override func viewWillAppear() {
         super.viewWillAppear()
