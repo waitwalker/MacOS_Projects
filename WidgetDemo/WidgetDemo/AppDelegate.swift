cb97602eb0745a0511fde0c73a7191b42942e3e0
waitwalker <waitwalker@163.com>
2020-03-30 09:15:58 +0800

    输入demo

---
+++ WidgetDemo/WidgetDemo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WidgetDemo
+//
+//  Created by etiantian on 2020/3/25.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WidgetDemo/WidgetDemo/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  WidgetDemo
+//
+//  Created by etiantian on 2020/3/25.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class TestView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
