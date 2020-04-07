c13b436ab8924b6f9a758e8d86b3e133483f08b5
waitwalker <waitwalker@163.com>
2020-04-07 15:07:56 +0800

    NSMenu

--- ToolBarDemo/ToolBarDemo/AppDelegate.swift
+++ ToolBarDemo/ToolBarDemo/AppDelegate.swift
@@ -11,12 +11,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
+    @IBAction func openAction(_ sender: NSMenuItem) {
+        
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
