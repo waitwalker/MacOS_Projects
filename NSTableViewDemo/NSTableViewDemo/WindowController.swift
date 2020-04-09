c8366511de2f8efcb32daaf7b47062a169286504
waitwalker <waitwalker@163.com>
2020-04-09 10:12:48 +0800

    tableView 容器创建

---
+++ NSTableViewDemo/NSTableViewDemo/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  NSTableViewDemo
+//
+//  Created by etiantian on 2020/4/9.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
