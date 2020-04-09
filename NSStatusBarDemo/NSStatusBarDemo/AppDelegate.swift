69d46f121ad77c62199c21158a4b143cfb35b55b
waitwalker <waitwalker@163.com>
2020-04-09 09:37:20 +0800

    status menu 添加

--- NSStatusBarDemo/NSStatusBarDemo/AppDelegate.swift
+++ NSStatusBarDemo/NSStatusBarDemo/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var item: NSStatusItem?
 
+    @IBOutlet weak var statusItemMenu: NSMenu!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let statusBar = NSStatusBar.system
         let statusItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
@@ -21,13 +23,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.button?.title = "按钮"
         self.item = statusItem
         
-        
+        statusItem.menu = statusItemMenu
     }
     
     @objc func itemAction(button:NSButton) -> Void {
-        
+        print("status item did click")
     }
-
+    
+    
+    @IBAction func item1Action(_ sender: NSMenuItem) {
+        print("item1Action")
+    }
+    
+    @IBAction func item2Action(_ sender: NSMenuItem) {
+        print("item2Action")
+    }
+    
+    @IBAction func item3Action(_ sender: NSMenuItem) {
+        print("item3Action")
+    }
+    
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         let statusBar = NSStatusBar.system
         statusBar.removeStatusItem(self.item!)
