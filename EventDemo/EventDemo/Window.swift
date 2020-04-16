712a3b177205b86f0d034f21aa78486159b8c72d
waitwalker <waitwalker@163.com>
2020-04-16 15:45:29 +0800

    鼠标事件

---
+++ EventDemo/EventDemo/Window.swift
@@ -0,0 +1,69 @@
+//
+//  Window.swift
+//  EventDemo
+//
+//  Created by etiantian on 2020/4/16.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class Window: NSWindow {
+
+    override func sendEvent(_ event: NSEvent) {
+        print("监听所有事件:\(event)")
+        super.sendEvent(event)
+    }
+    
+    
+    override func mouseDown(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("鼠标落下:\(locationPoint)")
+        
+        print("鼠标点击次数:\(event.clickCount)")
+        super.mouseDown(with: event)
+    }
+    
+    override func mouseUp(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("鼠标抬起:\(locationPoint)")
+        super.mouseUp(with: event)
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("鼠标退出:\(locationPoint)")
+        super.mouseExited(with: event)
+    }
+    
+    override func mouseMoved(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("鼠标移动:\(locationPoint)")
+        super.mouseMoved(with: event)
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("鼠标拖动:\(locationPoint)")
+        super.mouseDragged(with: event)
+    }
+    
+    override func rightMouseDown(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("右键鼠标落下:\(locationPoint)")
+        super.rightMouseDown(with: event)
+    }
+    
+    override func rightMouseUp(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("右键鼠标抬起:\(locationPoint)")
+        super.rightMouseUp(with: event)
+    }
+    
+    override func rightMouseDragged(with event: NSEvent) {
+        let locationPoint = event.locationInWindow
+        print("右键鼠标拖动:\(locationPoint)")
+        super.rightMouseDragged(with: event)
+    }
+    
+}
