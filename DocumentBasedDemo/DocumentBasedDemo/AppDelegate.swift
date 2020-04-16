7b34fd2276b496a0ea323e1ea84f52939f58b147
waitwalker <waitwalker@163.com>
2020-04-16 10:59:55 +0800

    手动连接Menu Item action 事件

--- DocumentBasedDemo/DocumentBasedDemo/AppDelegate.swift
+++ DocumentBasedDemo/DocumentBasedDemo/AppDelegate.swift
@@ -10,13 +10,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
+    @IBAction func menuItemAction(_ sender: NSMenuItem) {
+        print("点击word menu item")
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let wordCountWindowController = storyboard.instantiateController(withIdentifier: "Word Count Window Controller") as! NSWindowController
+        if let wordCountWindow = wordCountWindowController.window {
+            let wordCountVC = wordCountWindowController.contentViewController as! WordCountViewController
+            NSApplication.shared.runModal(for: wordCountWindow)
+            wordCountWindow.close()
+        }
+    }
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
