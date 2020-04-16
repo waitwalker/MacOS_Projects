d6d3bc2a3a17bbd3a8c8825f427d191429d0728c
waitwalker <waitwalker@163.com>
2020-04-16 09:03:50 +0800

    窗口文件创建

---
+++ WindowsDemo/WindowsDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WindowsDemo
+//
+//  Created by etiantian on 2020/4/15.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonAction(_ sender: NSButton) {
+        print("log 输出")
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ WindowsDemo/WindowsDemo/WindowController.swift
@@ -0,0 +1,18 @@
+//
+//  WindowController.swift
+//  WindowsDemo
+//
+//  Created by etiantian on 2020/4/15.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+    }
+
+}
