8f7846d7a78877262768ce18c635648724477554
waitwalker <waitwalker@163.com>
2020-04-16 09:54:14 +0800

    手动获取窗口对象&screen,并且调整窗口对象的frame

--- DocumentBasedDemo/DocumentBasedDemo/WindowController.swift
+++ DocumentBasedDemo/DocumentBasedDemo/WindowController.swift
@@ -18,7 +18,15 @@ class WindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
     
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        if let window = window, let screen = window.screen {
+            let offsetFromLeftOfScrren = CGFloat(150)
+            let offsetFromTopOfScreen = CGFloat(150)
+            let screenFrame = screen.visibleFrame
+            let offsetFromBottomOfScreen = screenFrame.maxY - window.frame.height - offsetFromTopOfScreen
+            window.setFrameOrigin(CGPoint(x: offsetFromLeftOfScrren, y: offsetFromBottomOfScreen))
+            
+        }
+        
     }
 
 }
