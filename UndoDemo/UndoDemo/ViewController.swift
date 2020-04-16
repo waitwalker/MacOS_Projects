f341342710d3294832530962c4a8d42a7770186c
waitwalker <waitwalker@163.com>
2020-04-16 17:30:11 +0800

    撤销与恢复视图创建完成

---
+++ UndoDemo/UndoDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  UndoDemo
+//
+//  Created by etiantian on 2020/4/16.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var textFieldOne: NSTextField!
+    @IBOutlet weak var textFieldTwo: NSTextField!
+    @IBOutlet weak var textFieldThree: NSTextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func computeAction(_ sender: NSButton) {
+        
+    }
+    
+    
+    @IBAction func undoAction(_ sender: NSButton) {
+        
+    }
+    
+    
+    @IBAction func redoAction(_ sender: NSButton) {
+        
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
