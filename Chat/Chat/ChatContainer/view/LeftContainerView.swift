c58f052c9222c6d666bad6a166fd1f1bbaaa2e46
waitwalker <waitwalker@163.com>
2020-04-22 16:42:33 +0800

    tableview 添加

--- Chat/Chat/ChatContainer/view/LeftContainerView.swift
+++ Chat/Chat/ChatContainer/view/LeftContainerView.swift
@@ -10,10 +10,45 @@ import Cocoa
 
 class LeftContainerView: NSView {
     
+    var scrollView: NSScrollView!
+    var tableView: NSTableView!
+    
+    var dataSource: [[String: Any]] = []
+    
+    
+    
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
+        setupSubviews()
+    }
+    
+    func setupDataSource() -> Void {
         
     }
+    
+    func setupSubviews() -> Void {
+        scrollView = NSScrollView()
+        scrollView.backgroundColor = NSColor.yellow
+        scrollView.hasVerticalScroller = true
+        self.addSubview(scrollView)
+        
+        scrollView.snp.makeConstraints { (make) in
+            make.left.right.bottom.top.equalTo(self)
+        }
+        
+        let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier.init(rawValue: "chat_column"))
+        column.title = "最近聊天"
+        tableView = NSTableView()
+        tableView.addTableColumn(column)
+        tableView.delegate = self
+        tableView.dataSource = self
+        scrollView.documentView = tableView
+        
+        tableView.snp.makeConstraints { (make) in
+            make.left.right.bottom.top.equalTo(scrollView)
+        }
+    }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -23,5 +58,54 @@ class LeftContainerView: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
     }
+    
+}
+
+extension LeftContainerView: NSTableViewDelegate, NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        var cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cellId"), owner: self)
+        if cell == nil {
+            cell = ChatCell(frame: NSRect(x: 0, y: 0, width: 270, height: 70))
+            cell!.identifier = NSUserInterfaceItemIdentifier(rawValue: "cellId")
+        }
+        (cell as! ChatCell).currentData = dataSource[row]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return 70
+    }
+    
+}
+
+class ChatCell: NSView {
+    
+    
+    
+    var currentData: [String: Any]? {
+        didSet {
+            
+        }
+    }
+    
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        setupSubviews()
+    }
+    
+    func setupSubviews() -> Void {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
 }
