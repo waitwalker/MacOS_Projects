5949732a8a5d20e5dc39176988e9d3e79e8f0dd3
waitwalker <waitwalker@163.com>
2020-04-23 15:21:37 +0800

    中间视图顶部容器初始化完成

--- Chat/Chat/ChatContainer/view/MiddleContainerView.swift
+++ Chat/Chat/ChatContainer/view/MiddleContainerView.swift
@@ -12,6 +12,12 @@ class MiddleContainerView: NSView {
 
     var leftBorder: NSView!
     var rightBorder: NSView!
+    var topContainerView: NSView!
+    var nameLabel: NSTextField!
+    var addGroupUserButton: NSButton!
+    var foldButton: NSButton!
+    
+    
 
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
@@ -42,6 +48,58 @@ class MiddleContainerView: NSView {
             make.right.top.bottom.equalTo(0)
             make.width.equalTo(0.5)
         }
+        
+        topContainerView = NSView()
+        topContainerView.wantsLayer = true
+        topContainerView.layer?.backgroundColor = NSColor.white.cgColor
+        self.addSubview(topContainerView)
+        topContainerView.snp.makeConstraints { (make) in
+            make.top.equalTo(0)
+            make.left.equalTo(2)
+            make.right.equalTo(-2)
+            make.height.equalTo(60)
+        }
+        
+        nameLabel = NSTextField()
+        nameLabel.stringValue = "python && Golang"
+        nameLabel.isEditable = false
+        nameLabel.textColor = NSColor.black
+        nameLabel.backgroundColor = NSColor.white
+        nameLabel.font = NSFont.systemFont(ofSize: 15)
+        nameLabel.sizeToFit()
+        nameLabel.isBordered = false
+        nameLabel.isBezeled = false
+        topContainerView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(20)
+            make.centerY.equalTo(topContainerView)
+        }
+        
+        foldButton = NSButton(image: NSImage(named: "add_new_chat")!, target: self, action: #selector(foldButtonAction))
+        topContainerView.addSubview(foldButton)
+        foldButton.snp.makeConstraints { (make) in
+            make.right.equalTo(-10)
+            make.height.width.equalTo(40)
+            make.centerY.equalTo(topContainerView)
+        }
+        
+        addGroupUserButton = NSButton(image: NSImage(named: "add_new_chat")!, target: self, action: #selector(addGroupUserButtonAction))
+        topContainerView.addSubview(addGroupUserButton)
+        addGroupUserButton.snp.makeConstraints { (make) in
+            make.right.equalTo(foldButton.snp.left).offset(-10)
+            make.height.width.equalTo(40)
+            make.centerY.equalTo(topContainerView)
+        }
+        
+        
+    }
+    
+    @objc func foldButtonAction() -> Void {
+        
+    }
+    
+    @objc func addGroupUserButtonAction() -> Void {
+        
     }
     
     override func draw(_ dirtyRect: NSRect) {
