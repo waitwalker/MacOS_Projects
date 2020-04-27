dfbf53994303e27506ccdb6c9e862ab72c7fb3a2
waitwalker <waitwalker@163.com>
2020-04-27 17:23:17 +0800

    toolbar 中间容器添加

---
+++ Chat/Chat/Toolbar/ToolbarMiddleContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ToolbarMiddleContentView.swift
+//  Chat
+//
+//  Created by etiantian on 2020/4/27.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+import Cocoa
+
+class ToolbarMiddleContentView: NSView {
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
