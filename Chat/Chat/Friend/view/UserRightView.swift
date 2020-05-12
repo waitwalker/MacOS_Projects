4d86ccecbee7ffe5b0ab5012c010d8ab28c0ee93
waitwalker <waitwalker@163.com>
2020-05-12 16:28:57 +0800

    用户相关模块添加占位图

--- Chat/Chat/Friend/view/UserRightView.swift
+++ Chat/Chat/Friend/view/UserRightView.swift
@@ -10,10 +10,32 @@ import Cocoa
 
 class UserRightView: NSView {
     
+    var iconImageView: NSImageView!
+    
+    
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         self.wantsLayer = true
         self.layer?.backgroundColor = NSColor.white.cgColor
+        
+        setupSeubviews()
+    }
+    
+    /*
+     * name: setupSeubviews
+     * description: 初始化子控件
+     * author: waitwalker
+     * date: 2020.05.11
+     */
+    private func setupSeubviews() -> Void {
+        iconImageView = NSImageView(image: NSImage(named: "user_placeholder")!)
+        self.addSubview(iconImageView)
+        
+        iconImageView.snp.makeConstraints { (make) in
+            make.center.equalTo(self)
+            make.height.equalTo(243)
+            make.width.equalTo(222)
+        }
     }
     
     required init?(coder: NSCoder) {
